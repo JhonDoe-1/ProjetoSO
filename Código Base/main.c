136d65740c5d31c72f3ab162867a0b6cb8bc4c0b
Tiago Almeida <tiago.c.almeida@tecnico.ulisboa.pt>
2023-12-10 14:17:07 +0000

    modified:   "C\303\263digo Base/constants.h" 	modified:   "C\303\263digo Base/main.c"

--- Código Base/main.c
+++ Código Base/main.c
@@ -2,89 +2,150 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #include "constants.h"
 #include "operations.h"
 #include "parser.h"
 
+int executeCommand(int command);
+char * removeSubStr(char * str,char * substr);
+
 int main(int argc, char *argv[]) {
   unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
-
+  int i=1;;
   if (argc > 1) {
     char *endptr;
     unsigned long int delay = strtoul(argv[1], &endptr, 10);
-
+    /*
     if (*endptr != '\0' || delay > UINT_MAX) {
       fprintf(stderr, "Invalid delay value or value too large\n");
       return 1;
     }
-
+    */
     state_access_delay_ms = (unsigned int)delay;
+
+
   }
 
   if (ems_init(state_access_delay_ms)) {
     fprintf(stderr, "Failed to initialize EMS\n");
     return 1;
   }
-
-  while (1) {
-    unsigned int event_id, delay;
-    size_t num_rows, num_columns, num_coords;
-    size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
-
+  if(argc>1){
+    if(strcmp(argv[1],"jobs")==0){
+      DIR *dirp;
+      struct dirent *dp;
+      dirp = opendir(argv[1]);
+      if (dirp == NULL) {
+        printf("opendir failed on '%s'", argv[1]);
+        /*return;*/
+      }
+      else{
+        for (;;) {
+          /*errno = 0;  To distinguish error from end-of-directory */
+          dp = readdir(dirp);
+          if (dp == NULL)
+            break;
+          if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+            continue; /* Skip . and .. */
+          
+          if(strstr(dp->d_name,".jobs")!=NULL){
+            int outfile,file;
+            char * outputName=strcat(removeSubStr(dp->d_name,".jobs"),".out");
+            outfile=open(outputName,O_WRONLY | O_CREAT, 0644);
+            file=open(dp->d_name, O_RDONLY);
+            if (outfile == -1) {
+              perror("open failed");
+              exit(1);
+            }
+            if (dup2(outfile, 1) == -1) {
+              perror("dup2 failed"); 
+              exit(1);
+            }
+            executeCommand(file);
+            close(outfile);
+            
+          }
+          
+          /*printf("%s\n", dp->d_name);*/
+        }
+      }
+    }
+  }
+  while (i==1) {
+    
     printf("> ");
     fflush(stdout);
 
-    switch (get_next(STDIN_FILENO)) {
+    i=executeCommand(STDIN_FILENO);
+    if(i==0){
+      return 0;
+    }
+  }
+}
+
+
+
+int executeCommand(int command){
+  unsigned int event_id, delay;
+    
+  size_t num_rows, num_columns, num_coords;
+  size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
+
+  switch (get_next(command)) {
       case CMD_CREATE:
-        if (parse_create(STDIN_FILENO, &event_id, &num_rows, &num_columns) != 0) {
+        if (parse_create(command, &event_id, &num_rows, &num_columns) != 0) {
           fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
+          
         }
 
         if (ems_create(event_id, num_rows, num_columns)) {
           fprintf(stderr, "Failed to create event\n");
         }
 
-        break;
+        return 1;
 
       case CMD_RESERVE:
-        num_coords = parse_reserve(STDIN_FILENO, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+        num_coords = parse_reserve(command, MAX_RESERVATION_SIZE, &event_id, xs, ys);
 
         if (num_coords == 0) {
           fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
+          
         }
 
         if (ems_reserve(event_id, num_coords, xs, ys)) {
           fprintf(stderr, "Failed to reserve seats\n");
         }
 
-        break;
+        return 1;
 
       case CMD_SHOW:
-        if (parse_show(STDIN_FILENO, &event_id) != 0) {
+        if (parse_show(command, &event_id) != 0) {
           fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
+          
         }
 
         if (ems_show(event_id)) {
           fprintf(stderr, "Failed to show event\n");
         }
 
-        break;
+        return 1;
 
       case CMD_LIST_EVENTS:
         if (ems_list_events()) {
           fprintf(stderr, "Failed to list events\n");
         }
 
-        break;
+        return 1;
 
       case CMD_WAIT:
-        if (parse_wait(STDIN_FILENO, &delay, NULL) == -1) {  // thread_id is not implemented
+        if (parse_wait(command, &delay, NULL) == -1) {  // thread_id is not implemented
           fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
+          
         }
 
         if (delay > 0) {
@@ -92,11 +153,11 @@ int main(int argc, char *argv[]) {
           ems_wait(delay);
         }
 
-        break;
+        return 1;
 
       case CMD_INVALID:
         fprintf(stderr, "Invalid command. See HELP for usage\n");
-        break;
+        return 1;
 
       case CMD_HELP:
         printf(
@@ -109,15 +170,37 @@ int main(int argc, char *argv[]) {
             "  BARRIER\n"                      // Not implemented
             "  HELP\n");
 
-        break;
+        return 1;
 
       case CMD_BARRIER:  // Not implemented
       case CMD_EMPTY:
-        break;
+        return 1;
 
       case EOC:
         ems_terminate();
         return 0;
     }
-  }
+    return 0;
 }
+
+char * removeSubStr(char * str, char *substr){
+    char *scan_p, *temp_p;
+    int subStrSize = 5;
+    if (str == NULL){
+        return 0;
+    }
+    else if (substr == NULL){
+        return str;
+    }
+    else if (strlen(substr)> strlen(str)){
+        return str;
+    }
+    temp_p = str;
+    while((scan_p = strstr(temp_p,substr))){
+        temp_p = scan_p + subStrSize;
+        memmove(scan_p, temp_p, sizeof(temp_p)+1);
+    }
+    return str;
+}
+
+
